6c9816381b2bce087626eb17fa745acc00873e0c
Jack Tanner <ugmo04@users.noreply.github.com>
2017-10-23 14:20:38 +0200

    Created draft Upgradeable.sol and necessarry dependancy contract files (ENS, Ownable, PublicResolver, SafeMath, strings)

---
+++ contracts/Upgradable.sol
@@ -0,0 +1,111 @@
+pragma solidity ^0.4.15;
+
+/*import 'https://github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol';*/
+/*import 'https://github.com/ethereum/ens/contracts/ENS.sol';
+import 'https://github.com/ethereum/ens/contracts/PublicResolver.sol';*/
+
+import './lib/Ownable.sol';
+import './lib/ENS.sol';
+import './lib/PublicResolver.sol';
+
+/**
+ * @title Upgradable
+ * @dev Contracts inheriting from Upgradeable can have their contract function calls routed to a new contract
+ * if the contract is upgraded
+ */
+contract Upgradable is Ownable {
+    ENS ens;
+    bytes32 node;
+
+    event nodeChanged(bytes32 newNode);
+
+    function Upgradable(ENS _ens, bytes32 _node) public {
+        ens = _ens;
+        node = _node;
+        nodeChanged(_node);
+    }
+
+    /**
+     * Sets a new ENS node to check for if an upgrade has been set
+     * @param newNode The ENS node.
+     */
+    function changeNode(bytes32 newNode) public onlyOwner {
+        nodeChanged(newNode);
+    }
+
+    /**
+     * Uses the current contract function if no upgrade has been set (through ENS)
+     * If an upgrade has been set, send the call to the new function
+     * The upgraded function must have the same interface
+     * Once upgraded, only the upgraded contract can access the old contract's methods
+     */
+    modifier upgradeable () {
+        address upgradedContract = PublicResolver(ens.resolver(node)).addr(node);
+
+        if (upgradedContract != address(this)) {
+            if (upgradedContract == msg.sender) {
+                // Allow the new upgraded contract to still make calls, if needed
+                _;
+            } else {
+                // Route the message to the new contract
+                upgradedContract.delegatecall(msg.data);
+            }
+        } else {
+            // Execute as normal
+            _;
+        }
+    }
+}
+
+contract Wallet {
+    event Deposit(address deposit, uint amount);
+    event Withdrawal(address withdrawal, uint amount);
+    function deposit() public payable;
+    function withdrawal() public;
+}
+
+contract SimpleWalllet is Wallet, Upgradable {
+    mapping (address => uint) public balances;
+    // What will happen to the state of the new contract???
+
+    event Deposit(address deposit, uint amount);
+    event Withdrawal(address withdrawal, uint amount);
+
+    function SimpleWalllet(ENS _ens, bytes32 _node) Upgradable(_ens, _node) public {}
+
+    function deposit() upgradeable public payable {
+        balances[msg.sender] += msg.value;
+        Deposit(msg.sender, msg.value);
+    }
+
+    function withdrawal() upgradeable public {
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        Withdrawal(msg.sender, balances[msg.sender]);
+        msg.sender.transfer(amount);
+
+    }
+}
+
+contract UpgradedWallet is Wallet, Upgradable {
+    mapping (address => uint) public balances;
+    SimpleWalllet simplewallet;
+
+    function UpgradedWallet(ENS _ens, bytes32 _node, SimpleWalllet _simplewallet) Upgradable(_ens, _node) {
+        simplewallet = _simplewallet;
+    }
+
+    function deposit() upgradeable public payable {
+        if (msg.value < 1 ether) revert(); // Upgrade contract so that only deposits of 1 Ether or more are accepted
+        balances[msg.sender] += msg.value;
+        Deposit(msg.sender, msg.value);
+    }
+
+    function withdrawal() upgradeable public {
+        uint amount =   balances[msg.sender];
+        balances[msg.sender] = 0;
+        Withdrawal(msg.sender, balances[msg.sender]);
+        msg.sender.transfer(amount);
+
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,15 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ENS = artifacts.require("./lib/ENS.sol");
+var Ownable = artifacts.require("./lib/Ownable.sol");
+var PublicResolver = artifacts.require("./lib/PublicResolver.sol");
+var Upgradable = artifacts.require("./Upgradable.sol");
+
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(Ownable);
+  // deployer.link(Ownable, Upgradable);
+  // deployer.deploy(PublicResolver);
+  // deployer.link(PublicResolver, Upgradable);
+  // deployer.deploy(ENS);
+  // deployer.link(ENS, Upgradable);
+  deployer.deploy(Upgradable);
 };
